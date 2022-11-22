a96d3c0a59174dd4650d6224f415f5028c933ece
Marcus <60523971+Simpada@users.noreply.github.com>
2022-11-22 15:21:20 +0100

    Update to test stuff

--- server/__tests__/restaurantApi.test.js
+++ server/__tests__/restaurantApi.test.js
@@ -2,8 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import request from "supertest";
 import bodyParser from "body-parser";
-import {MongoClient} from "mongodb";
-import {RestaurantApi} from "../restaurantApi.js";
+import { MongoClient } from "mongodb";
+import { RestaurantApi } from "../restaurantApi.js";
 
 dotenv.config();
 
@@ -12,63 +12,63 @@ app.use(bodyParser.json());
 const mongoClient = new MongoClient(process.env.MONGODB_URL);
 
 beforeAll(async () => {
-    await mongoClient.connect();
-    const database = mongoClient.db("exam_test_db");
-    await database.collection("menu").deleteMany({});
-    app.use("/api/menu", RestaurantApi(database));
+  await mongoClient.connect();
+  const database = mongoClient.db("exam_test_db");
+  await database.collection("menu").deleteMany({});
+  app.use("/api/menu", RestaurantApi(database));
 });
 afterAll(() => {
-    mongoClient.close();
+  mongoClient.close();
 });
 
 describe("Restaurant Api", () => {
+  it("adds and gets item", async () => {
+    await request(app)
+      .post("/api/menu/add")
+      .send({
+        name: "test dish",
+        ingredients: ["test", "dish"],
+        price: 9000,
+        category: "fake",
+      })
+      .expect(200);
+    expect(
+      (await request(app).get("/api/menu").expect(200)).body.map(
+        ({ name }) => name
+      )
+    ).toContain("test dish");
+  });
 
-    it("adds and gets item", async () => {
-        await request(app)
-            .post("/api/menu/add")
-            .send({
-                name: "test dish",
-                ingredients: ["test", "dish"],
-                price: 9000,
-                category: "fake"
-            }).expect(200);
-        expect(
-            (await request(app).get("/api/menu").expect(200)).body.map(
-                ({name}) => name
-            )
-        ).toContain("test dish");
-    });
+  it("shows filtered items by category", async () => {
+    await request(app)
+      .post("/api/menu/add")
+      .send({
+        name: "test dish",
+        ingredients: ["test", "dish"],
+        price: 9000,
+        category: "fake",
+      })
+      .expect(200);
 
-    it("shows filtered items by category", async () => {
-        await request(app)
-            .post("/api/menu/add")
-            .send({
-                name: "test dish",
-                ingredients: ["test", "dish"],
-                price: 9000,
-                category: "fake"
-            }).expect(200);
+    await request(app)
+      .post("/api/menu/add")
+      .send({
+        name: "other",
+        ingredients: ["test", "dish"],
+        price: 200,
+        category: "boring",
+      })
+      .expect(200);
 
-        await request(app)
-            .post("/api/menu/add")
-            .send({
-                name: "other",
-                ingredients: ["test", "dish"],
-                price: 200,
-                category: "boring"
-            }).expect(200);
-
-
-        expect(
-            (await request(app).get("/api/menu?category=fake").expect(200)).body.map(
-                ({name}) => name
-            )
-        ).toContain("test dish");
-        expect(
-            (await request(app).get("/api/menu?category=fake").expect(200)).body.map(
-                ({name}) => name
-            )
-        ).not.toContain("other");
-    });
-
+    expect(
+      (await request(app).get("/api/menu?category=fake").expect(200)).body.map(
+        ({ name }) => name
+      )
+    ).toContain("test dish");
+    expect(
+      (await request(app).get("/api/menu?category=fake").expect(200)).body.map(
+        ({ name }) => name
+      )
+    ).not.toContain("other");
+  });
 });
